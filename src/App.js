99802118458f25aeaa1960ab7a228f8707a36096
Michał Adamczyk <hello@codeman.it>
2021-08-07 20:54:58 +0200

    add theme.js, ThemeProvider and VoteButton component

--- src/App.js
+++ src/App.js
@@ -1,38 +1,52 @@
 import React from "react";
-import styled from "styled-components";
-import {memesList} from "./database/database.js";
+import styled, { ThemeProvider } from "styled-components";
+import { memesList } from "./database/database.js";
+import { theme } from "./theme"
 
+const VoteButton = styled.button`
+  border: 2px solid #e3e3e3;
+  border-radius: 10px;
+  box-shadow: 2px 2px 5px 0px #ccc;
+  padding: 10px 20px;
+  margin: 10px;
+  background: ${({theme}) => theme.colors.white};
+  color: ${({ color }) => color || "black"};
+  font-weight: bold;
+  transition: 0.4s ease-in-out;
+
+  &:hover{
+    background: ${({ color }) => color==="red" ? "red" : "green"};
+    color: ${({theme}) => theme.colors.white};
+    cursor: pointer;
+  }
+`;
 
 const MemeList = (props) => (
-
   <>
-  <ul>
-     {props.memes.map(mem => {
+    <ul style={{ listStyleType: "none" }}>
+      {props.memes.map((mem) => {
+        const { img } = mem;
 
-      const {img} = mem;
-      
-      return (
-        <li>
-          UP: {mem.upvotes}, DOWN: {mem.downvotes}
-
-          <div>
-            <img  src={img}  alt="img"/>
-          </div>
-        </li>
-      )
-     
-    })} 
-  </ul>
+        return (
+          <li key={mem.id}>
+            <div>
+              <img src={img} alt="img" />
+            </div>
+            <VoteButton color="green">👍 Upvote: {mem.upvotes} </VoteButton>
+            <VoteButton color="red">👎 Downvote: {mem.downvotes}</VoteButton>
+          </li>
+        );
+      })}
+    </ul>
   </>
 );
 
-
 function App() {
   return (
-    <>
-    <p>ELO</p>
-    <MemeList memes={memesList}/>
-    </>
+    <ThemeProvider theme={theme}>
+      <p>MEMES LIST</p>
+      <MemeList memes={memesList} />
+    </ThemeProvider>
   );
 }
 
---
+++ src/theme.js
@@ -0,0 +1,30 @@
+export const theme = {
+    colors: {
+        teal: "#008080",
+        tealDark: "#007070",
+        lightGrey: "#ccc",
+        concrete: "#F2F2F2",
+        red: "red",
+        green: "green",
+        lack: "#000000",
+        woodsmoke: "#18181B",
+        stormGray: "#74788B",
+        blue: "#0044CC",
+        plugGray: "#C4C4C4",
+        darkGrey: "#333333",
+        darkerGrey: "#7E839A",
+        yellow: "#FCD420",
+        mediumBlue: "#59CBEE",
+        lightBlue: "#D6E4FF",
+        grey: "#E4E6F0",
+        lightGrey: "#F5F5FA",
+        white: "#FFFFFF"
+    },
+
+    breakpoints: {
+        large: "1384px",
+        big: "1036px",
+        medium: "890px",
+        mobile: "768px",
+    }
+};
